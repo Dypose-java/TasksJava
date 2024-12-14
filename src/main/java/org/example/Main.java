97e3c5711333680d446336e1ea9670333145fc1a
Dypose-java <d.bolonkin0022@gmail.com>
2024-12-14 17:49:15 +0300

    add commit..

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,14 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+    int i = 0;
+    //fsdfsdfs
+        //sdfsdf
+
+
+
+
+
     }
+
 }
