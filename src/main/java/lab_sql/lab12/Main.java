b05b68323e6cadc87c58d09cc735cf7d0a92ce8d
dypose <d.bolonkin22@mail.ru>
2025-04-22 22:00:25 +0300

    add finish task robot

---
+++ src/main/java/lab_sql/lab12/Main.java
@@ -0,0 +1,7 @@
+package lab_sql.lab12;
+
+public class Main {
+    public static void main(String[] args) {
+        new Students().getTaskNumberOne();
+    }
+}
---
+++ src/main/java/leetcode/programmingSkills/RobotBoundedInCircle/Main.java
@@ -0,0 +1,11 @@
+package leetcode.programmingSkills.RobotBoundedInCircle;
+
+public class Main {
+static Robot robot = new Robot("");
+
+    public static void main(String[] args) {
+        robot.straight();
+        robot.straight();;
+        String points = robot.toString();
+    }
+}
